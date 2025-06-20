3cebd956e9a623a90c3cd17904a73d161eb15032
goeun441 <20200298@dongduk.ac.kr>
2025-06-20 22:28:05 +0900

    [FEAT] 제한 시간 60초 적용

---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject timerText;
+    float time = 60.0f;
+
+    void Start()
+    {
+        this.timerText = GameObject.Find("Time");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.time -= Time.deltaTime;
+        this.timerText.GetComponent<TextMeshProUGUI>().text = this.time.ToString("F1");
+
+        if (time <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
